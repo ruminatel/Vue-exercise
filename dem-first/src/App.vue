3604a98ace4ce263a38acbd0ad0e7ac3578f016c
game <rongjdai@163.com>
2022-08-16 11:43:34 +0800

    vue

--- dem-first/src/App.vue
+++ dem-first/src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <p>{{username}}</p>
+    <button @click="changName">点击</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+        username : 'zx'
+    }
+  },
+  methods : {
+    changName () {
+      console.log(this);
+      this.username = 'pink'
+    }
   }
 }
 </script>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style>
+div {
+  height: 200px;
+  background-color: aqua;
 }
 </style>
