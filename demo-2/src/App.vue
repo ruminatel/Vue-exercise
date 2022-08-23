aff53b35a947d2b3a8ba4e0994187921b9479ef4
game <rongjdai@163.com>
2022-08-23 11:39:43 +0800

    vue-ref

--- demo-2/src/App.vue
+++ demo-2/src/App.vue
@@ -9,6 +9,10 @@
       <Left :msg="message" :user="userinfo"></Left>
       <Right @numchangs="getnewcount"></Right>
     </div>
+
+    <hr>
+    <input type="text" v-if="inputshow" @blur="showbutton" ref="iptRef">
+    <button v-else @click="showinput">点击显示 input</button>
   </div>
 </template>
 
@@ -21,7 +25,8 @@ export default {
       message : 'hello 123',
       userinfo : {username : 'zx', age : 18},
       // 定义 countFromSon 来接收子组件传递过来的数据
-      countfromson: 0
+      countfromson: 0,
+      inputshow : false
     }
   },
   components: {
@@ -32,6 +37,15 @@ export default {
      // 获取子组件传递过来的数据
     getnewcount(val) {
       this.countfromson = val
+    },
+    showinput() {
+      this.inputshow = true
+      this.$nextTick(() => {
+       this.$refs.iptRef.focus()
+      })
+    },
+    showbutton() {
+      this.inputshow = false
     }
   }
 }
