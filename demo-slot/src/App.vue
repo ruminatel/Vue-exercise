6a7871c378eb4bfaaeca04ffb0868108156ba6cf
game <rongjdai@163.com>
2022-09-23 11:36:43 +0800

    slot

---
+++ demo-slot/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="app-container">
+    <h1>App 根组件</h1>
+
+    
+
+    <div class="box">
+      <!-- 渲染 Left 组件和 Right 组件 -->
+      <!-- 3. 以标签形式，使用注册好的组件 -->
+      
+        <Left>
+          <template v-slot:default>
+            <p>这里是在left组件的内容区域，声明的p标签</p>
+          </template>
+        </Left>
+      
+     
+    </div>
+  </div>
+</template>
+
+<script>
+// 1. 导入需要使用的 .vue 组件
+import Left from '@/components/Left.vue'
+import Right from '@/components/Right.vue'
+
+export default {
+  // 2. 注册组件
+  components : {
+    Left,
+    Right
+  }
+}
+</script>
+
+<style lang="less">
+.app-container {
+  padding: 1px 20px 20px;
+  background-color: #efefef;
+}
+.box {
+  display: flex;
+}
+</style>
---
+++ demo-slot/src/components/Left.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="left-container">
+    <h3>Left 组件</h3>
+    <hr />
+   
+   <!-- 定义插槽区域 -->
+   <slot name="default">
+      <h3>这个是插槽的默认内容</h3>
+   </slot>
+
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+.left-container {
+  padding: 0 20px 20px;
+  background-color: orange;
+  min-height: 250px;
+  flex: 1;
+}
+
+h3 {
+  color: red;
+}
+
+// h5[data-v-3c83f0b7]
+// [data-v-3c83f0b7] h5
+
+// 当使用第三方组件库的时候，如果有修改第三方组件默认样式的需求，需要用到 /deep/
+/deep/ h5 {
+  color: pink;
+}
+</style>
