1d2d7a6db44476dd734a1724ee0edd8cda053031
game <rongjdai@163.com>
2022-10-18 11:17:12 +0800

    article

---
+++ article/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <keep-alive include="Home">
+      <router-view></router-view>
+    </keep-alive>
+
+    <van-tabbar route>
+      <van-tabbar-item icon="home-o" to="/">首页</van-tabbar-item>
+      <van-tabbar-item icon="user-o" to="/user">我的</van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="less" scoped></style>
