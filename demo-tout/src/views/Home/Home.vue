2dbbc01774480ba45e84081972c34a8b706a56a6
game <rongjdai@163.com>
2022-11-01 11:51:40 +0800

    tout

---
+++ demo-tout/src/views/Home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home-container">
+    <van-nav-bar title="头条" fixed />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HoMe'
+}
+</script>
+
+<style lang='less' scoped>
+.home-container {
+  padding: 46px 0 50px 0;
+}
+</style>
